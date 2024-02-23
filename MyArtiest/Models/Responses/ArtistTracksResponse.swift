4201f8e2ce71ad2f9d0006abc9c200bd861c61b7
Mike Phan <khoaphan90@gmail.com>
2024-02-22 21:59:25 -0500

    added UI for artist page and add new artist functionality from home screen

---
+++ MyArtiest/Models/Responses/ArtistTracksResponse.swift
@@ -0,0 +1,8 @@
+//
+//  ArtistTracksResponse.swift
+//  MyArtiest
+//
+//  Created by Mike Phan on 2/22/24.
+//
+
+import Foundation
---
+++ MyArtiest/ViewModels/ArtistViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  MyArtiest
+//
+//  Created by Mike Phan on 2/22/24.
+//
+
+import Foundation
